950147fff0b6940fd529d10cc81ac9838849bae3
Oleh <prodius.oleg@icloud.com>
2023-10-03 20:56:03 +0200

    second commit

---
+++ src/main/java/com/prodius/model/Book.java
@@ -0,0 +1,24 @@
+package com.prodius.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Book {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+
+    private String name;
+
+    private String author;
+}
